6813ea6b8e991e6a858500ee413866bf488a2a4d
Ben <928691436@qq.com>
2024-11-22 11:55:50 +0800

    feat: 初始化及fiber实现

---
+++ src/ReactFiber.js
@@ -0,0 +1,26 @@
+import { FunctionComponent, HostComponent } from "./ReactWorkTags"
+import { isStr, Placement } from "./utils"
+
+export function createFiber(vnode, returnFiber) {
+    const fiber = {
+        type: vnode.type,
+        key: vnode.key,
+        props: vnode.props,
+        stateNode: null, // 原生标签对应dom节点,class组件对应实例,函数组件对应null
+        child: null, // 第一个子fiber
+        sibling: null, // 下一个兄弟节点
+        return: returnFiber,
+        flags: Placement,
+        index: null,
+    }
+
+
+    const { type } = vnode
+
+    if (isStr(type)) { // dom
+        fiber.tag = HostComponent
+    } else if (isFn(type)) { // 函数组件以及类组件
+        fiber.tag = FunctionComponent
+    }
+    return fiber
+}
